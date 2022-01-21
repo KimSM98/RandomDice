198a180ede52c2baf2dba11b2fbdb783ded51d76
ksm987 <kimsomin92@gmail.com>
2022-01-21 23:17:57 +0900

    SpawnPosition, 다이스 랜덤 위치 생성
    SpawnPosition
    : Board의 Child 오브젝트이며 다이스를 생성할 위치를 가지고 있다.
    
    다이스 랜덤 위치 생성
    : DiceSpawner에서 SpawnDice로 다이스를 랜덤 위치에 생성할 수 있다.

--- RandomDice/Assets/Scripts/DiceSpawner.cs
+++ RandomDice/Assets/Scripts/DiceSpawner.cs
@@ -6,11 +6,33 @@ public class DiceSpawner : MonoBehaviour
 {
     public Dice dicePrefab;
     public DiceType[] diceTypes;
+    public SpawnPosition spawnPos;
+
+    private int numOfDice;
+
+    private void Start()
+    {
+        numOfDice = 0;
+    }
 
     public void SpawnDice()
     {
-        Dice dice = Instantiate(dicePrefab);
-        dice.InitDice(diceTypes[0]);
+        if(numOfDice >= 15)
+        {
+            Debug.Log("소환 가능한 다이스의 최대 개수에 도달했습니다.");
+            return;
+        }
+        
+        // Random Position
+        Vector2 randPos = spawnPos.GetRandomEmptyPos();
+        
+        Dice dice = Instantiate(dicePrefab, randPos, Quaternion.identity);
+
+        // Random types
+        int randNum = Random.Range(0, diceTypes.Length);
+        dice.InitDice(diceTypes[randNum]);
+
+        numOfDice++;
     }
 
 }
---
+++ RandomDice/Assets/Scripts/SpawnPosition.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPosition : MonoBehaviour
+{
+    public float xOffset;
+    public float yOffset;
+
+    private List<Vector2> emptyPos;
+    private List<Vector2> activePos;
+
+    bool afterStart = false; // for Gizmos
+
+    void Start()
+    {
+        emptyPos = new List<Vector2>();
+        activePos = new List<Vector2>();
+
+        InitSpawnPosition();
+
+        afterStart = true;
+    }
+
+    private void InitSpawnPosition()
+    {
+        Vector2 startPos = transform.position;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                Vector2 pos = new Vector2(startPos.x + xOffset * j, startPos.y - yOffset * i);
+                emptyPos.Add(pos);
+            }
+        }
+    }
+
+    public Vector2 GetRandomEmptyPos()
+    {
+        int randNum = Random.Range(0, emptyPos.Count);
+        Vector2 pos = emptyPos[randNum];
+        emptyPos.RemoveAt(randNum);
+        activePos.Add(pos);
+
+        return pos;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Vector2 size = new Vector3(0.1f, 0.1f);
+
+        Gizmos.DrawCube(transform.position, size);
+
+        if (!afterStart) return;
+
+        Gizmos.color = Color.green;
+        foreach (Vector2 pos in emptyPos)
+        {
+            Gizmos.DrawCube(pos, size);
+        }
+
+        Gizmos.color = Color.magenta;
+        foreach (Vector2 pos in activePos)
+        {
+            Gizmos.DrawCube(pos, size);
+        }
+    }
+}
